3d5bdc14905fd9b7ff55c05287e5dba755037349
peerlevi <orzelek@hm.edu>
2025-01-07 13:01:17 +0100

    added some logging setups and path organisation

--- src/models/train_and_save_model.py
+++ src/models/train_and_save_model.py
@@ -1,26 +1,57 @@
+import os 
+import logging
+import pathlib
+from src.utils.project_paths import find_project_root  # Importiere die Funktion aus der Utils-Datei
+
 import numpy as np
 import tensorflow as tf
-import os
 from tensorflow.keras.utils import image_dataset_from_directory
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+
 
 # Hilfsfunktionen für die Pfadmanipulation
 ####################################################
 # Absoluter Pfad zu diesem Skript
 current_file_path = os.path.abspath(__file__)
 # Projekt-Root relativ zu diesem Skript
-project_root = os.path.dirname(current_file_path)
-print("Projekt-Root:", project_root)
+PROJECT_ROOT = find_project_root(Path(__file__))
+# Definition der wichtigsten Verzeichnisse
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+SRC_DIR = os.path.join(PROJECT_ROOT, "src")
+MODELS_DIR = os.path.join(PROJECT_ROOT, "models")
 ####################################################
 
+# Logging-Konfiguration
+####################################################
+logging.basicConfig(level=logging.INFO)
 
-# Datenverzeichnis
-dataset_dir = "Wildfire"  # Passe den Pfad an
+# Pfad für Log-Datei
+LOG_DIR = os.path.join(PROJECT_ROOT, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)  # Verzeichnis erstellen, falls es nicht existiert
+LOG_FILE = os.path.join(LOG_DIR, "project.log")
+# Logging-Konfiguration
+logging.basicConfig(
+    level=logging.INFO,  # Log-Level: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    format="%(asctime)s - %(name)s - [Line: %(lineno)d] - %(module)s - %(funcName)s - %(levelname)s - %(message)s"
+    # %(asctime)s: Zeitstempel, %(name)s: Logger-Name, [Line: %(lineno)d]: Zeilennummer im Code,
+    # %(levelname)s: Log-Level (INFO, WARNING, etc.), %(message)s: Log-Nachricht
+    handlers=[
+        logging.FileHandler(LOG_FILE),  # Logs in Datei schreiben
+        logging.StreamHandler()         # Logs in der Konsole ausgeben
+    ]
+)
+log = logging.getLogger(__name__)
+log.info("Projekt-Root:", PROJECT_ROOT)
+####################################################
+
+# Trainings- und Validierungsdatensätze Pfad erstellen - Pathlib
+dataset_dir = os.path.join(DATA_DIR, "processed", "Wildfire")
 
 # Überprüfen, ob das Dataset existiert
 if not os.path.exists(dataset_dir):
-    print(f"Fehler: Das Verzeichnis '{dataset_dir}' wurde nicht gefunden.")
+    log.error(f"Fehler: Das Verzeichnis '{dataset_dir}' wurde nicht gefunden.")
 else:
     # Trainings- und Validierungsdatensätze erstellen
     train_dataset = image_dataset_from_directory(
@@ -41,8 +72,8 @@ else:
         batch_size=32
     )
     
-    class_names = train_dataset.class_names
-    print("Klassen:", class_names)
+    class_names = train_dataset.class_names # Klassenamen extrahieren, Klassennamen sind die Namen der Unterordner
+    log.info("Klassen:", class_names)
 
     # Modell erstellen
     model = Sequential([
